0ccfdd296ac157c56c4538bee89bfb9cf896d90d
Nuryagdy Azadov <74148048+Nuryagdy1407@users.noreply.github.com>
2023-11-29 11:35:33 +0500

    5min projects)

---
+++ Autoamtion_for_ID(Soha)/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Autoamtion_for_ID_Soha_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(90, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Arduinony birikdiriň!";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(90, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(342, 87);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "2. .txt formata ýazyljak ID-lar aşakda      görüner! Her cardy bir gezek         " +
+    "       degirmegiňizi haýyş edýäris!";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(90, 248);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 68);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "ID";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(352, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = " ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Autoamtion_for_ID_Soha_.Properties.Resources.icons8_delete_480px;
+            this.pictureBox2.Location = new System.Drawing.Point(356, 248);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(76, 68);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Autoamtion_for_ID_Soha_.Properties.Resources.icons8_delete_480px;
+            this.pictureBox1.Location = new System.Drawing.Point(303, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 36);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(233, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 61);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(580, 567);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Autoamtion_for_ID";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Autoamtion_for_ID(Soha)/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Autoamtion_for_ID_Soha_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PortBarlaweDak()
+        {
+            serialPort1.Close();
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine(ports);
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                serialPort1.PortName = port;
+                label4.Text = serialPort1.PortName.ToString() + " detected!";
+                label4.ForeColor = Color.Green;
+                pictureBox1.Image = Properties.Resources.icons8_checkmark_480px_1;
+            }
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Arduinony birikdir!");
+                label4.Text = " ";
+                pictureBox1.Image = Properties.Resources.icons8_delete_480px;
+                Form1_Load(MessageBoxButtons.OKCancel, null);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PortBarlaweDak();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = @"D:\ID.txt";
+            File.AppendAllLines(path, new[] { "Checked!".ToString() });
+            label3.Text = "Checked!".ToString();
+            pictureBox2.Image = Properties.Resources.icons8_checkmark_480px_1;
+            button1.BackColor = Color.Green;
+        }
+
+        private void serialPort1_DataReceived_1(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = serialPort1.ReadLine();
+            data.Trim();
+            long k = Int64.Parse(data);
+            if (this.InvokeRequired)
+            {
+                Invoke(new MethodInvoker(delegate ()
+                {
+                    yazdyr(k);
+                }));
+            }
+        }
+
+        private void yazdyr(long data)
+        {
+            string path = @"D:\ID.txt";
+            File.AppendAllLines(path, new[] { data.ToString() });
+            label3.Text = data.ToString();
+            pictureBox2.Image = Properties.Resources.icons8_checkmark_480px_1;
+        }
+
+    }
+}
